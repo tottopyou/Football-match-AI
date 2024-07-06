cfbf1a8da21c8c6971a06b4250b1191c735f6f23
tottopyou <rgrabovsky2006@gmail.com>
2024-07-06 16:33:06 +0200

    connecting all functions

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+import cv2
+
 from utils import read_video,save_video
 from trackers import Tracker
+from teams import TeamAssigner
 def main():
 
     print("Reading the video")
     video_frames = read_video('Input_video/08fd33_4.mp4')
     print("Tracked the video")
-    tracker = Tracker('models/best.pt')
+    tracker = Tracker('models/best_second.pt')
 
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
+
+    teams = TeamAssigner()
+    teams.assign_team_color(video_frames[0],tracks['players'][0])
+
+    for frame_num, player_track in enumerate(tracks["players"]):
+        for player_id, track in player_track.items():
+            team = teams.get_player_team(video_frames[frame_num],track['bbox'],player_id)
+            tracks['players'][frame_num][player_id]['team'] = team
+            tracks['players'][frame_num][player_id]['team_color'] = teams.team_colors[team]
+
+
     print("Drawing annotations")
     output_video_frames = tracker.draw_annotations(video_frames,tracks)
     print("Saving the video")
